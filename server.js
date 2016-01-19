67a1579752e748668368362fd9e8d21b50025b1b
Ganjar Widiatmansyah <rajapulau@gmail.com>
2016-01-19 09:21:16 +0700

    change port

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ var config           = require('./webpack.config');
 
 new WebpackDevServer(webpack(config), {
     publicPath : config.output.publicPath,
-}).listen(3001, 'localhost', function(err, result) {
+}).listen(3000, 'localhost', function(err, result) {
     if(err){
         console.log(err);
     }
-    console.log('Listening at localhost:3001');
+    console.log('Listening at localhost:3000');
 });
